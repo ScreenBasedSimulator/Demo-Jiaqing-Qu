c4777b7f1ad3e995db7c5105d56361dcf2de0ca6
Jiaqing Qu <jiaqingq@andrew.cmu.edu>
2016-02-03 00:18:48 -0500

    move body demo

---
+++ Assets/OR_Room/Script/ControlDoctor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ControlDoctor : MonoBehaviour {
+	GameObject rightArm;
+	GameObject leftArm;
+	//GameObject rightForeArm = GameObject.Find("RightForeArm");
+
+
+
+	// Use this for initialization
+	void Start () {
+		rightArm = GameObject.Find("/Characters/Surgeon_A_Cloth/Reference/Hips/Spine/Spine1/Spine2/RightShoulder/RightArm");
+		leftArm = GameObject.Find("/Characters/Surgeon_A_Cloth/Reference/Hips/Spine/Spine1/Spine2/LeftShoulder/LeftArm");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			rightArm.transform.rotation =  Quaternion.Euler(30f, 176.6f, 44.7f);
+			leftArm.transform.rotation =  Quaternion.Euler(312f, 1.2f, 224.7f);
+
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			rightArm.transform.rotation =  Quaternion.Euler(351f, 176.6f, 44.7f);
+			leftArm.transform.rotation =  Quaternion.Euler(8.8f, 1.2f, 224.7f);
+		}
+	}
+	
+
+}
---
+++ Assets/OR_Room/Script/ControlDoctorByAnimation.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlDoctorByAnimation : MonoBehaviour {
+	GameObject microscope;
+
+	// Use this for initialization
+	void Start () {
+		microscope = GameObject.Find ("/Devices/Microscope");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			GetComponent<Animation> ().PlayQueued("M_Sit_Microdown", QueueMode.PlayNow);
+			microscope.GetComponent<Animation> ().Play ();
+			GetComponent<Animation> ().PlayQueued("M_Sit_Idle", QueueMode.CompleteOthers);
+
+		} else if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+//			GetComponent<Animation> ().Play ("M_Sit_Idle_Headturn");
+			GetComponent<Animation> ().PlayQueued("M_Sit_Idle_Headturn", QueueMode.PlayNow);
+			GetComponent<Animation> ().PlayQueued("M_Sit_Idle", QueueMode.CompleteOthers);
+
+		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+//			GetComponent<Animation> ().Play ("M_Sit_Anestesia");
+			GetComponent<Animation> ().PlayQueued("M_Sit_Anestesia", QueueMode.PlayNow);
+			GetComponent<Animation> ().PlayQueued("M_Sit_Idle", QueueMode.CompleteOthers);
+
+		}
+
+
+	
+	}
+}
